4305ecf5c3ccb07437b69cec31a96665a3f868f5
Beau Nouvelle <beau@getswifty.dev>
2023-03-13 15:52:33 +1100

    Initial Commit

---
+++ ParticlesSwiftUI/ParticlesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticlesSwiftUIApp.swift
+//  ParticlesSwiftUI
+//
+//  Created by Beau Nouvelle on 13/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParticlesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
