9e28cd87cd4b3ab4c16f7967ca7ccbae263dda21
Beau Nouvelle <beau@getswifty.dev>
2023-03-13 17:11:30 +1100

    finished

--- ParticlesSwiftUI/ContentView.swift
+++ ParticlesSwiftUI/ContentView.swift
@@ -6,17 +6,88 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class ParticleScene: SKScene {
+
+    override init(size: CGSize) {
+        super.init(size: size)
+
+        backgroundColor = .clear
+
+        if let emitter1 = SKEmitterNode(fileNamed: "MyParticle") {
+            emitter1.position.y = size.height
+            emitter1.particleColorSequence = nil
+            emitter1.particleColorBlendFactor = 1
+            emitter1.particleColorBlueRange = 1
+            emitter1.particleColorGreenRange = 1
+            emitter1.particleColorRedRange = 1
+            emitter1.position.x = size.width
+            addChild(emitter1)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
 
 struct ContentView: View {
+
+    @State var isPuchased = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            if isPuchased {
+                GeometryReader { geo in
+                    SpriteView(scene: ParticleScene(size: geo.size), options: [.allowsTransparency])
+                }
+            }
+
+            VStack {
+
+                Text("Unlock Premium")
+                    .font(.largeTitle)
+
+                Spacer()
+
+                if isPuchased {
+                    Text("🎉 Congratulations! 🎉")
+                } else {
+                    Text("Subscribe now to unlock all the features in the app!")
+                }
+
+                Spacer()
+
+                Button {
+                    withAnimation {
+                        isPuchased.toggle()
+                    }
+                } label: {
+                    Text("Purchase")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.primary)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.green))
+                }
+                .buttonStyle(ScaleButtonStyle())
+            }
+            .padding()
+
+
         }
-        .padding()
+
     }
+}
+
+struct ScaleButtonStyle: ButtonStyle {
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(x: configuration.isPressed ? 0.98 : 1, y: configuration.isPressed ? 0.98 : 1)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
